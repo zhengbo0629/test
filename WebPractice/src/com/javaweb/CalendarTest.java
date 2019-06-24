47a31de7630cca0515ddb769e58af08f5e93aabd
zhengbonuli521@163.com <zhengbonuli521@163.com>
2019-06-25 00:12:30 +0800

    zhengbo2  改结构

---
+++ WebPractice/src/com/javaweb/CalendarTest.java
@@ -0,0 +1,41 @@
+package com.javaweb;
+
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest {
+		public static void main(String[] args) {
+			// create calendar objects.
+		      Calendar cal = Calendar.getInstance();
+		      Calendar future = Calendar.getInstance();
+		      DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		    
+		      // print the current date
+		      System.out.println("Current date: " +  df.format(cal.getTime()));
+
+		      // change year in future calendar
+		      future.set(Calendar.YEAR, 2020);
+		      System.out.println("Year is " + future.get(Calendar.YEAR));
+
+		      // check if calendar date is after current date
+		      Date time = future.getTime();
+		      String format = df.format(time);
+		      if (future.after(cal)) {
+		         System.out.println("Date= " + format + " is after current date.");
+		      }
+
+		      
+		      DecimalFormat ds=(DecimalFormat)NumberFormat.getInstance(); 
+				ds.setMaximumFractionDigits(2);
+		      
+		      
+		      
+		   }
+
+
+		}
+
---
+++ WebPractice/src/com/javaweb/DataTypeEnum.java
@@ -0,0 +1,38 @@
+package com.javaweb;
+
+public enum DataTypeEnum {
+	VARCHAR("22","q","b"),
+	DOUBLE("DOUBLE","",""),
+	Int("Int","","");
+
+	
+    private final String name;
+    private final String group;
+    private final String chinese;
+    	
+  
+    DataTypeEnum(String name,String group,String chinese) {
+        this.name = name;
+        this.group=group;
+        this.chinese=chinese;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getGroup(){
+    	return group;
+    }
+    public String getChinese(){
+    	return chinese;
+    }
+    public static void main(String[] args) {
+		for (DataTypeEnum data :DataTypeEnum.values()) {
+			System.out.println(data.toString(data));
+		}
+	}
+    public String toString(DataTypeEnum temp) {
+    	return temp.chinese+";"+temp.group;
+    }
+
+}
